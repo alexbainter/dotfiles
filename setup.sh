45a1f39b14f5de1048b50578b7bef9a50c8f1953
metalex9 <alexbainter@gmail.com>
2021-08-16 13:15:23 -0500

    Fix cloned repo location path

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 {
   mkdir -p ~/source
   mkdir -p ~/.config
-  git clone --depth=1 https://github.com/metalex9/dotfiles.git ~/.source/dotfiles
+  git clone --depth=1 https://github.com/metalex9/dotfiles.git ~/source/dotfiles
   ln -s ~/source/dotfiles/nvim ~/.config/nvim
 }
